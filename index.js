a88e7240df45a50490dbfa49c23b1e22b8cfb975
Alistair <alistaircraig393@gmail.com>
2024-03-15 12:07:31 +0000

    First commit. Made the global variables and make the random word gen.

---
+++ index.js
@@ -0,0 +1,9 @@
+const firstName = ['Zeus', 'Apollo', 'Poseidon', 'Ares', 'Hades', 'Morpheus', 'Hermes', 'Chronos', 'Athena', 'Artemis '];
+const secondName = ['God', 'Goddess', 'Deity', 'Creator', 'Divine being', 'Avatar', 'Supreme being', 'Ruler', 'Lord', 'Spirit'];
+const thirdName = ['Love', 'Earth', 'Night', 'Fire', 'Lightning', 'War', 'Shadow', 'Dreams', 'Death', 'Life'];
+let finalName = [];
+
+const randomword = () => {
+    return Math.floor(Math.random() * 10);
+}
+
