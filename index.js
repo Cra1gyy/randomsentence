13c682f2706f221e743cc13eb5020069ee91382b
Alistair <alistaircraig393@gmail.com>
2024-03-15 13:22:05 +0000

    finished game. Added all of the random name genoraters and had them come together for a output

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const firstName = ['Zeus', 'Apollo', 'Poseidon', 'Ares', 'Hades', 'Morpheus', 'Hermes', 'Chronos', 'Athena', 'Artemis '];
+const firstName = ['Zeus', 'Apollo', 'Poseidon', 'Ares', 'Hades', 'Morpheus', 'Hermes', 'Chronos', 'Athena', 'Artemis'];
 const secondName = ['God', 'Goddess', 'Deity', 'Creator', 'Divine being', 'Avatar', 'Supreme being', 'Ruler', 'Lord', 'Spirit'];
 const thirdName = ['Love', 'Earth', 'Night', 'Fire', 'Lightning', 'War', 'Shadow', 'Dreams', 'Death', 'Life'];
-let finalName = [];
 
-const randomword = () => {
-    return Math.floor(Math.random() * 10);
+
+
+const firstNameGen = () => {
+
+    let gen1 = firstName[Math.floor(Math.random() * firstName.length)];
+
+    return gen1;
 }
 
+const secondNameGen = () => {
+
+    let gen2 = secondName[Math.floor(Math.random() * secondName.length)];
+
+    return gen2;
+}
+
+const thirdNameGen = () => {
+
+    let gen3 = thirdName[Math.floor(Math.random() * thirdName.length)];
+
+    return gen3;
+}
+
+
+
+const godSentence = () => {
+    console.log(firstNameGen() + ' ' + secondNameGen() + ' of ' + thirdNameGen());
+}
+
+
+godSentence();
